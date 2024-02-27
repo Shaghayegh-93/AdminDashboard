705351527b9d30730063f1ec48b15f690e172a8c
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-27 14:13:22 +0000

    add Footer component for Dashbord

---
+++ src/ui/dashboard/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-between mt-6 text-text-color-soft">
+      <span className="font-bold">Shaghayegh</span>
+      <span className="text-sm">© All right reserved</span>
+    </div>
+  );
+};
+
+export default Footer;
