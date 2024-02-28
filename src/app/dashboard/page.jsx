04893fd0720e2cff3a1ffe6b1e15113d591d68d7
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-28 14:02:57 +0000

    create dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,5 +1,25 @@
+import Card from "@/ui/dashboard/card/Card";
+import Chart from "@/ui/dashboard/chart/Chart";
+import RightBar from "@/ui/dashboard/rightbar/RightBar";
+import Transactions from "@/ui/dashboard/transactions/Transactions";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className=" flex gap-5 mt-5">
+      <div className=" basis-3/4 flex flex-col gap-5">
+        <div className="flex gap-5 justify-between">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className=" basis-1/4">
+        <RightBar />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
