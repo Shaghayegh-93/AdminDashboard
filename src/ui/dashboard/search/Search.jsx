3da7f33e45bc887778e6f3340cd0cf985c4f25b9
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-02-29 13:23:37 +0000

    add serachHandler function

--- src/ui/dashboard/search/Search.jsx
+++ src/ui/dashboard/search/Search.jsx
@@ -1,8 +1,22 @@
 "use client";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const searchHandler = (e) => {
+    const params = new URLSearchParams(searchParams);
+    if (e.target.value) {
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params}`);
+  };
   return (
     <div>
       <div className="flex items-center bg-[#2e374a] p-3 gap-2 rounded-md cursor-pointer">
@@ -11,6 +25,7 @@ const Search = ({ placeholder }) => {
           type="text"
           placeholder={placeholder}
           className="outline-none border-none bg-transparent text-text-color placeholder:text-text-color"
+          onChange={searchHandler}
         />
       </div>
     </div>
