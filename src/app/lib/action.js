089f1b63ec93fad90b8e5eecb3869f0e180ae5ee
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-03-01 12:22:27 +0000

    hash password with library  before send it to backend

--- src/app/lib/action.js
+++ src/app/lib/action.js
@@ -1,16 +1,19 @@
 "use server";
 import { revalidatePath } from "next/cache";
-import { User } from "./models";
+import { Product, User } from "./models";
 import { connectTodb } from "./utils";
 import { redirect } from "next/dist/server/api-utils";
-// import { ToastContainer, toast } from "react-toastify";
+import bcrypt from "bcrypt";
+
 
 export const addUser = async (formData) => {
   console.log("formdata", formData);
   const data = Object.fromEntries(formData);
   try {
     connectTodb();
-    const newUser = new User(data);
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(data.password, salt);
+    const newUser = new User({ ...data, password: hashPassword });
 
     await newUser.save();
   } catch (error) {
@@ -19,5 +22,48 @@ export const addUser = async (formData) => {
   }
   revalidatePath("/dashboard/users");
   redirect("/dashboard/users");
+};
+
+export const deleteUser = async (formData) => {
+  console.log("formdata", formData);
+  const { id } = Object.fromEntries(formData);
+  try {
+    connectTodb();
+    await User.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+  revalidatePath("/dashboard/users");
+};
+
+export const addProduct = async (formData) => {
+  console.log("formdata", formData);
+  const data = Object.fromEntries(formData);
+  try {
+    connectTodb();
+
+    const newProduct = new Product(data);
+
+    await newProduct.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
   //   toast.success("successs");
+};
+
+export const deleteProduct = async (formData) => {
+  console.log("formdata", formData);
+  const { id } = Object.fromEntries(formData);
+  try {
+    connectTodb();
+    await Product.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+  revalidatePath("/dashboard/products");
 };
