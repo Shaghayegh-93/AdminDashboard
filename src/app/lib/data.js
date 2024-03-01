c53a83eccfefefd15d5f25ede1a5031da3338e2b
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-03-01 10:39:22 +0000

    expand code

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,12 +1,32 @@
-import { User } from "./models";
+import { Product, User } from "./models";
 import { connectTodb } from "./utils";
 
-export const fetchUsers = async (q) => {
+export const fetchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
   try {
     connectTodb();
-    const users = await User.find({ username: { $regex: regex } });
-    return users;
+    const count = await User.find({ username: { $regex: regex } }).count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { users, count };
+  } catch (err) {
+    console.log(err);
+    throw new Error("error");
+  }
+};
+
+export const fetchProducts = async (q, page) => {
+  const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
+  try {
+    connectTodb();
+    const count = await Product.find({ title: { $regex: regex } }).count();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { products, count };
   } catch (err) {
     console.log(err);
     throw new Error("error");
